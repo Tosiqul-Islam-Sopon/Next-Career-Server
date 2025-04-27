2c9ecb087efea40aab051bd3fedc9fd5c336224a
Tosiqul-Islam-Sopon <sopon.tosiq@gmail.com>
2025-04-27 16:12:21 +0600

    fix: client link are added in cors to prevent cors errors

--- index.js
+++ index.js
@@ -12,7 +12,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://next-career-9a82c.web.app",
+    "https://next-career-9a82c.firebaseapp.com",
+  ]
+}));
 
 const server = http.createServer(app);
 
